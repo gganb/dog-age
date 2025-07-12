5dd4124e9c1147be5c5ce28ab0a6db16aebd650f
gganb <gksql0801@naver.com>
2025-07-12 14:32:26 +0900

    init  : 초기 커밋

---
+++ src/main/java/com/github/ggab/puppyage/PuppyAgeApplication.java
@@ -0,0 +1,13 @@
+package com.github.ggab.puppyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyAgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyAgeApplication.class, args);
+	}
+
+}
